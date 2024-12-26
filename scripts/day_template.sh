f4d1e48185902cacd07fae8b4c43c49d382d17cb
James Hoctor <JEHoctor@protonmail.com>
2024-12-25 19:45:58 -0500

    Update day templating to deal with complex number names

--- scripts/day_template.sh
+++ scripts/day_template.sh
@@ -7,10 +7,12 @@ PYTHON_INTERPRETER=$3
 
 read -p "What is the puzzle name? " PUZZLE_NAME
 read -p "What is the day number? " DAY_NUMERAL
-DAY_NUMBER=$($PYTHON_INTERPRETER -c "import inflect; p = inflect.engine(); print(p.number_to_words('$DAY_NUMERAL').replace('-', ''))")
+DAY_NUMBER=$($PYTHON_INTERPRETER -c "import inflect; p = inflect.engine(); print(p.number_to_words('$DAY_NUMERAL'))")
+DAY_NUMBER_WITH_UNDERSCORES=$(tr ' -' '_' <<< $DAY_NUMBER)
 
 NEW_FILE=$CMD_DIRECTORY/$DAY_NUMERAL.go
 cp $TEMPLATE_PATH $NEW_FILE
 sed -i "s/{{DAY_NUMBER}}/$DAY_NUMBER/g" $NEW_FILE
+sed -i "s/{{DAY_NUMBER_WITH_UNDERSCORES}}/$DAY_NUMBER_WITH_UNDERSCORES/g" $NEW_FILE
 sed -i "s/{{DAY_NUMERAL}}/$DAY_NUMERAL/g" $NEW_FILE
 sed -i "s/{{PUZZLE_NAME}}/$PUZZLE_NAME/g" $NEW_FILE
